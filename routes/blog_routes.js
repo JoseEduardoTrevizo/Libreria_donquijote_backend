d966a73339ca7c4a08f1d2ce06186224ced72e49
EduardoTrevizo <Edwardo_tp@hotmail.com>
2025-10-22 14:36:54 -0600

    se cambio el nombre de la ruta de la peticion GET en blog_routes

--- routes/blog_routes.js
+++ routes/blog_routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const BlogController = require("../controller/blog");
 
 router.post("/nueva_recomendacion", BlogController.crearEntrada); // Crear entrada de blog
-router.get("/", BlogController.obtenerEntradas); // Listar entradas de blog
+router.get("/recomendaciones", BlogController.obtenerEntradas); // Listar entradas de blog
 router.delete("/eliminar_entrada/:id", BlogController.eliminarEntrada); // Eliminar entrada de blog
 router.put("/editar_entrada/:id", BlogController.editarEntrada); // Editar entrada de blog
 
